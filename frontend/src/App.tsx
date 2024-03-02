2ec253afde07f7cc3930ed0431ea3902342a70df
ErivanOliveira <erivanoliveiraviana@gmail.com>
2024-03-02 00:29:21 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-function app () {
-return (
-    <h1>ola mundo!</h1>
-)
+import NotificationButton from "./components/NotificationButton"
+
+function app() {
+    return (
+        <><h1>ola mundo!</h1>
+        <NotificationButton /></>
+    )
 }
 
 export default app
